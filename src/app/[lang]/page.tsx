d8eea2567e8770eeae227fbf7b48b68695526d75
Natan Hopkin <natanhopkin@Natans-MacBook-Air.local>
2023-12-01 16:56:46 +0100

    sentry added

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,9 +1,19 @@
+'use client';
+
 import HomeHero from './home/homeHero';
 
 export default function Home() {
   return (
     <>
       <HomeHero />
+      <button
+        type="button"
+        onClick={() => {
+          throw new Error('Sentry Frontend Error');
+        }}
+      >
+        Throw error
+      </button>
     </>
   );
 }
